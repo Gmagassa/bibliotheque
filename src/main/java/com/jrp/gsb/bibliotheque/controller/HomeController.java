442653888f4865cb60b8cd98e83ec1c8b975f927
Ganda Magassa <magassa.ganda@gmail.com>
2023-12-06 23:09:10 +0000

    bibliotheque

--- src/main/java/com/jrp/gsb/bibliotheque/controller/HomeController.java
+++ src/main/java/com/jrp/gsb/bibliotheque/controller/HomeController.java
@@ -1,39 +1,26 @@
 package com.jrp.gsb.bibliotheque.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jrp.gsb.bibliotheque.entities.Editor;
 import com.jrp.gsb.bibliotheque.repository.EditorRepository;
 
 
-
 @Controller
-@RequestMapping("/editors")
-public class EditorCotroller {
+@RequestMapping
+public class HomeController {
     @Autowired
     EditorRepository editorRepo;
-    @GetMapping("/lists")
+    @GetMapping("/")
     public String displayEditor(Model model){
         List<Editor> editor = editorRepo.findAll();
         model.addAttribute("editorList", editor);
         return "home";
     }
-    @GetMapping("/new")
-    public String addNewEditor(Model model) {
-        Editor editor = new Editor();
-        model.addAttribute("editors", editor);
-        return "editors/new-editors";
-    }
-    @PostMapping("/save")
-    public String addEditor(Editor editor, Model model){
-        editorRepo.save(editor);
-        return "redirect:/editors/new";
-    }
-    
 }
